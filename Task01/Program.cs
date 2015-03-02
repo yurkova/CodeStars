648404abc495e99c7f0bb462570def43bf1a8ee2
Viktoriia Yurkova <viyurkova@gmail.com>
2015-03-02 18:36:15 +0200

    Modified Task01 to accept various input data

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,25 +1,29 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Numerics;
 
 namespace Task01
 {
     static class Program
     {
-        const int GirlsCount = 104;
-        const int SecondsPerPermutation = 10;
         const int SecondsInHour = 3600;
 
         static void Main()
         {
+            Console.WriteLine("Please, enter the number of girls:");
+            var girlsCount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the time in seconds for one arrangement:");
+            var secondsPerPermutation = Convert.ToInt32(Console.ReadLine());
+
             // The number of permutations of n distinct objects is n factorial.
             BigInteger permutationCount = 1;
-            for (var i = GirlsCount; i > 0; i--)
+            for (var i = girlsCount; i > 0; i--)
             {
                 permutationCount *= i;
             }
 
             BigInteger elapsedHours = permutationCount *
-                SecondsPerPermutation / SecondsInHour;
+                secondsPerPermutation / SecondsInHour;
 
             using (var sw = new StreamWriter(@"..\..\ANSWER.md"))
             {

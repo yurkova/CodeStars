f519d6445e1a25e079ed486958f181a5db094132
Viktoriia Yurkova <viyurkova@gmail.com>
2015-03-19 15:23:13 +0200

    Added Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Task07
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Please, enter the path to the input file:");
+            var path = Console.ReadLine();
+            string text;
+            if (path != null)
+                using (var sr = new StreamReader(path))
+                {
+                    text = sr.ReadToEnd();
+                }
+            else
+            {
+                Console.WriteLine("Path was not specified.");
+                return;
+            }
+            using (var sw = new StreamWriter(@"..\..\ANSWER.md"))
+            {
+                sw.WriteLine(GetOwhershipPercent(text));
+            }
+        }
+
+        private static int GetOwhershipPercent(string text)
+        {
+            const string firstPattern = @"[^xyzw]*";
+            const string secondPattern = @"[^abcd]*";
+            var firstMaxLength = Regex.Matches(text, firstPattern,
+                RegexOptions.IgnoreCase)
+                .Cast<Match>()
+                .Aggregate(0,
+                    (current, match) =>
+                        (match.Length > current)
+                            ? match.Length
+                            : current);
+            var secondMaxLength = Regex.Matches(text, secondPattern,
+                RegexOptions.IgnoreCase)
+                .Cast<Match>()
+                .Aggregate(0,
+                    (current, match) =>
+                        (match.Length > current)
+                            ? match.Length
+                            : current);
+            return (int)Math.Round((double)secondMaxLength * 100
+                                    / (firstMaxLength + secondMaxLength));
+        }
+    }
+}

7a93e6275e0dac406a44237afdc3f559d0083ac5
Viktoriia Yurkova <viyurkova@gmail.com>
2015-03-20 23:12:13 +0200

    Added Task09

---
+++ Task09/EvaluationEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task09
+{
+    internal class EvaluationEntry
+    {
+        public EvaluationEntry(string line)
+        {
+            var values = line.Split(new[] {' '},
+                StringSplitOptions.RemoveEmptyEntries);
+            Judge = values[2];
+            if (Convert.ToInt32(values[0]) < 0)
+            {
+                Penalty = Convert.ToInt32(values[0]);
+                Score = Convert.ToInt32(values[1]);
+                MessyEntry = false;
+            }
+            else
+            {
+                Penalty = Convert.ToInt32(values[1]);
+                Score = Convert.ToInt32(values[0]);
+                MessyEntry = true;
+            }
+        }
+
+        public int Penalty { get; private set; }
+        public int Score { get; private set; }
+        public string Judge { get; private set; }
+        public bool MessyEntry { get; private set; }
+    }
+}
---
+++ Task09/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Task09
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var entries = new List<EvaluationEntry>();
+            Console.WriteLine("Please, enter the path to input file:");
+            var path = Console.ReadLine();
+            if (path != null)
+            {
+                using (var sr = new StreamReader(path))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        entries.Add(new EvaluationEntry(line));
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Path was not specified.");
+                return;
+            }
+            using (var sw = new StreamWriter(@"..\..\ANSWER.md"))
+            {
+                sw.WriteLine(GetResultsOfCompetition(entries));
+            }
+        }
+
+        private static string GetResultsOfCompetition(
+            List<EvaluationEntry> entries)
+        {
+            var averagePenalty = (int) Math.Ceiling(
+                entries.Average(e => e.Penalty));
+            var totalScore = entries.Select(e => e.Score).Sum();
+
+            var judgesMistakesCount = new Dictionary<string, int>();
+            foreach (var entry in entries)
+            {
+                if (!entry.MessyEntry) continue;
+                if (judgesMistakesCount.ContainsKey(entry.Judge))
+                {
+                    judgesMistakesCount[entry.Judge]++;
+                }
+                else
+                {
+                    judgesMistakesCount.Add(entry.Judge, 0);
+                }
+            }
+            var carelessJudge = judgesMistakesCount.First(x =>
+                x.Value == judgesMistakesCount.Values.Max()).Key;
+
+            return averagePenalty + " " + totalScore + " " + carelessJudge;
+        }
+    }
+}

ba766305430e703083664b48fb3641d5247e8538
Viktoriia Yurkova <viyurkova@gmail.com>
2015-03-21 14:27:56 +0200

    Added Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Task13
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var bricksCount = 0;
+            Console.WriteLine("Please, enter the number of stairs:");
+            var stairsCount = Convert.ToInt32(Console.ReadLine());
+            for (var i = 1; i <= stairsCount; i++)
+            {
+                bricksCount += i;
+            }
+            using (var sw = new StreamWriter(@"..\..\ANSWER.md"))
+            {
+                sw.WriteLine(bricksCount);
+            }
+        }
+    }
+}
